cb26b66f96e83f6dd5977e699857f11dba238c70
rahulsarin.exe@gmail.com <rahulsarin.exe@gmail.com>
2020-06-25 20:20:32 +0530

    Bubble Sorting Program Added

---
+++ src/test/java/com/example/project/sorting/SortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.sorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
